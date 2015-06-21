ccec6df65d6256e9a70f21646bc14ac2c71b456a
Richard Carhart <richard.carhart@hansoninc.com>
2015-06-21 18:35:38 -0400

    Adding detail view

--- app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivityFragment.java
@@ -18,6 +18,8 @@ public class DetailActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_detail, container, false);
     }
 }
